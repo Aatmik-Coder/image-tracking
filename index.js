13c64580cc08967d6c101c3241384fb8c359e836
Aatmik-Coder <coderaatmik@gmail.com>
2022-11-21 17:51:32 +0530

    added new 3d models to assets folder and index.js file for viewing each 3d models

---
+++ index.js
@@ -0,0 +1,302 @@
+function fun1() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/buster_drone/scene.gltf" scale="150 150 150" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun2() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/car/scene.gltf" scale="1500 1500 1500" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun3() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/monk/scene.gltf" scale="250 250 250" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun4() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/DevParisar.glb" scale="5 5 5" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun5() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/Fridge.glb" scale="150 150 150" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun6() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/tree.gltf" scale="15 15 15" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun7() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/wooden_wardrobe.glb" scale="10 10 10" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun8() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/Couch.gltf" scale="25 25 25" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun9() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/Microwave.glb" scale="25 25 25" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun10() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/coffee_table.gltf" scale="150 150 150" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun11() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/arm_chair__furniture.glb" scale="250 250 250" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun12() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/chair1.glb" scale="150 150 150" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun13() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/ImageToStl.com_2 bhk.glb" scale="15 15 15" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun14() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/ImageToStl.com_kitchen.glb" scale="15 15 15" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun15() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/couldron/scene.gltf" scale="250 250 250" position="800 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
+
+function fun16() {
+    document.getElementById('cam').innerHTML = `
+    <a-scene 
+        vr-mode-ui="enabled: false;" 
+        renderer="logarithmicDepthBuffer: true;" 
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;">
+        <a-nft type="nft" url="assets/image_descriptors/track_image" smooth="true" smoothCount="10" smoothTolerance=".01"
+            smoothThreshold="5">
+            <a-entity gltf-model="assets/building/scene.gltf" scale="5 5 5" position="400 0 -100">
+            </a-entity>
+        </a-nft>
+        
+        <a-entity camera>
+
+        </a-entity>
+    </a-scene>`
+}
